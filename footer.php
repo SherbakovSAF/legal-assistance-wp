ac6da936899e0220a9470aca894a35d76861d926
SherbakovSAF <sherbakovsaf@gmail.com>
2023-09-09 12:20:49 +0300

    Вынесение логотипа в админку WP

---
+++ footer.php
@@ -0,0 +1,27 @@
+<footer>
+          <div class="footer-info large-container">
+               <div class="logo-block-wrap">
+                    <?php the_custom_logo(); ?>
+                    <abbr title="International Legal Center">International</br>Legal Center</abbr>
+                    <p>Первый цифровой международный юридический центр</p>
+               </div>
+               <div class="footer-info_social-media">
+                    <a href="#"><img src="../assets/images/vk.svg" alt="vk"></a>
+                    <a href="#"><img src="../assets/images/instagram.svg" alt="instagram"></a>
+                    <a href="#"><img src="../assets/images/twitter.svg" alt="twitter"></a>
+                    <a href="#"><img src="../assets/images/facebook.svg" alt="facebook"></a>
+               </div>
+          </div>
+
+          <div class="confidentiality-wrap">
+               <div class="large-container">
+                    <blockquote>©2021 «International Legal Center» | Разработано: effex-it.com</blockquote>
+                    <div class="confidentiality-wrap_links">
+                         <a href="#">Политика конфиденциальности</a>
+                         <a href="#">Пользовательское соглашение</a>
+                    </div>
+               </div>
+               
+          </div>
+     </footer>
+     <!-- <?php wp_footer(); ?> -->
--- functions.php
+++ functions.php
@@ -1,67 +1,25 @@
 <?php
-/**
- * Twenty Twenty-Two functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_Two
- * @since Twenty Twenty-Two 1.0
- */
+add_action('wp_enqueue_scripts', 'linking_style');
+add_action('after_setup_theme', 'theme_register');
+add_filter( 'upload_mimes', 'extra_mime_types' );
+
+function extra_mime_types( $mimes ) {
+	$mimes['svg'] = 'image/svg+xml';
+	return $mimes;
+}
 
 
-if ( ! function_exists( 'twentytwentytwo_support' ) ) :
+function theme_register(){
+	register_nav_menu('headerNav', 'Меню в header');
+	// Logo
+	remove_theme_support( 'custom-logo' );
+	add_theme_support( 'custom-logo');
+	// Logo-end
+}
 
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * @since Twenty Twenty-Two 1.0
-	 *
-	 * @return void
-	 */
-	function twentytwentytwo_support() {
-
-		// Add support for block styles.
-		add_theme_support( 'wp-block-styles' );
-
-		// Enqueue editor styles.
-		add_editor_style( 'style.css' );
-
-	}
-
-endif;
-
-add_action( 'after_setup_theme', 'twentytwentytwo_support' );
-
-if ( ! function_exists( 'twentytwentytwo_styles' ) ) :
-
-	/**
-	 * Enqueue styles.
-	 *
-	 * @since Twenty Twenty-Two 1.0
-	 *
-	 * @return void
-	 */
-	function twentytwentytwo_styles() {
-		// Register theme stylesheet.
-		$theme_version = wp_get_theme()->get( 'Version' );
-
-		$version_string = is_string( $theme_version ) ? $theme_version : false;
-		wp_register_style(
-			'twentytwentytwo-style',
-			get_template_directory_uri() . '/style.css',
-			array(),
-			$version_string
-		);
-
-		// Enqueue theme stylesheet.
-		wp_enqueue_style( 'twentytwentytwo-style' );
-
-	}
-
-endif;
-
-add_action( 'wp_enqueue_scripts', 'twentytwentytwo_styles' );
-
-// Add block patterns
-require get_template_directory() . '/inc/block-patterns.php';
+function linking_style(){
+	wp_enqueue_style('style', get_stylesheet_uri());
+	wp_enqueue_style('roboto', get_template_directory_uri().'/assets/fonts/Roboto/stylesheet.css');
+	wp_enqueue_style('gotham', get_template_directory_uri().'/assets/fonts/GothamPro/stylesheet.css');
+	wp_enqueue_style('main', get_template_directory_uri().'/assets/css/main.css');
+}
