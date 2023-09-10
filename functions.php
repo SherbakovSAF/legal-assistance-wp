0e6800104822519e4f40fb8eb882741952dad7ba
SherbakovSAF <sherbakovsaf@gmail.com>
2023-09-10 16:10:34 +0300

    Тарифы привязаны к админке WordPress

--- functions.php
+++ functions.php
@@ -1,23 +1,64 @@
 <?php
 add_action('wp_enqueue_scripts', 'linking_style');
 add_action('after_setup_theme', 'theme_register');
+add_action('init', 'register_post_types');
+
+
+
 add_filter( 'upload_mimes', 'extra_mime_types' );
+
+add_theme_support( 'post-thumbnails');
+add_theme_support( 'custom-logo');
+add_theme_support( 'title-tag');
+
+function register_post_types(){
+	register_post_type( 'tariffs', [
+		'label'  => null,
+		'labels' => [
+			'name'               => 'Тарифы', // основное название для типа записи
+			'singular_name'      => 'Тариф', // название для одной записи этого типа
+			'add_new'            => 'Добавить тариф', // для добавления новой записи
+			'add_new_item'       => 'Добавление тарифа', // заголовка у вновь создаваемой записи в админ-панели.
+			'edit_item'          => 'Редактирование тарифа', // для редактирования типа записи
+			'new_item'           => 'Новый тариф', // текст новой записи
+			'view_item'          => 'Смотреть тариф', // для просмотра записи этого типа.
+			'search_items'       => 'Искать тариф', // для поиска по этим типам записи
+			'not_found'          => 'Не найдено', // если в результате поиска ничего не было найдено
+			'not_found_in_trash' => 'Не найдено в корзине', // если не было найдено в корзине
+			'parent_item_colon'  => '', // для родителей (у древовидных типов)
+			'menu_name'          => 'Тарифы', // название меню
+		],
+		'description'            => 'Тарифы оказания услуг',
+		'public'                 => true,
+		'publicly_queryable'  => true, // зависит от public
+		'exclude_from_search' => true, // зависит от public
+		'show_ui'             => true, // зависит от public
+		'show_in_nav_menus'   => true, // зависит от public
+		'show_in_menu'           => true, // показывать ли в меню админки
+		'show_in_admin_bar'   => true, // зависит от show_in_menu
+		'show_in_rest'        => true, // добавить в REST API. C WP 4.7
+		'rest_base'           => null, // $post_type. C WP 4.7
+		'menu_position'       => 4,
+		'menu_icon'           => 'dashicons-cart',
+		//'capability_type'   => 'post',
+		//'capabilities'      => 'post', // массив дополнительных прав для этого типа записи
+		//'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
+		'hierarchical'        => false,
+		'supports'            => [ 'title', 'editor','custom-fields'],
+		'taxonomies'          => [],
+		'has_archive'         => false,
+		'rewrite'             => true,
+		'query_var'           => true,
+	] );
+}
 
 function extra_mime_types( $mimes ) {
 	$mimes['svg'] = 'image/svg+xml';
 	return $mimes;
 }
 
-function theme_register_nav_menu(){
-	register_nav_menu('headerNav', 'Меню в header');
-}
-
 function theme_register(){
 	register_nav_menu('headerNav', 'Меню в header');
-	// Logo
-	remove_theme_support( 'custom-logo' );
-	add_theme_support( 'custom-logo');
-	// Logo-end
 }
 
 function linking_style(){
---
+++ home.php
@@ -0,0 +1,335 @@
+<?php
+/*
+Template Name: home
+*/
+?>
+<?php get_header(); ?>
+<div class="header-title">
+     <h1 class="text-title">Юридический полис ILC — быстрое решение сложных вопросов 24/7</h1>
+     <p class="text-p-title">Ваша личная команда профессиональных юристов, которая всегда под рукой. А также доступ к бонусам партнерской программы — доход до $260 с каждого полиса, купленного по вашему приглашению.</p>
+     <div>
+          <a class="btn" href="#">Попробовать бесплатно</a>
+     </div>
+</div>
+<div class="header-circle-el">
+
+     <div>
+          <p>
+               Доход
+               <span>до $260</span>
+               <span>с каждого</br>полиса</span>
+          </p>
+          <img src="<?php echo wp_get_upload_dir()['url'] . '/header-circle-el.svg' ?>" alt="header-circle-el">
+     </div>
+</div>
+<div class="header-img">
+     <img src="<?php echo wp_get_upload_dir()['url'] . '/header-main-img.svg' ?>" alt="header-main-img">
+</div>
+<div class="header-advantages text-header-advantages">
+     <div class="header-advantages__wrap">
+          <dl>
+               <dt>
+                    <img src="<?php echo wp_get_upload_dir()['url'] . '/header-advantages-support.svg' ?>" alt="header-advantages-support">
+               </dt>
+               <dd>Юридическая поддержка 24/7</dd>
+          </dl>
+          <dl>
+               <dt>
+                    <img src="<?php echo wp_get_upload_dir()['url'] . '/header-advantages-possibilities.svg' ?>" alt="header-advantages-possibilities">
+               </dt>
+               <dd>Множество возможностей в одном полисе</dd>
+          </dl>
+          <dl>
+               <dt>
+                    <img src="<?php echo wp_get_upload_dir()['url'] . '/header-advantages-bonus.svg' ?>" alt="header-advantages-bonus">
+               </dt>
+               <dd>Бонусы партнерской программы</dd>
+          </dl>
+     </div>
+</div>
+</header>
+<main>
+     <section class="support small-container">
+          <div class="support-about">
+               <div class="support-about-title">
+                    <div class="logo-block-wrap">
+                         <?php the_custom_logo(); ?>
+                    </div>
+                    <h2 class="text-support-title">Ваша личная online команда юристов</h2>
+                    <p class="text-support-subtitle">Мы предоставляем комплексный консалтинговый сервис с полисной системой обслуживания для частных клиентов и для бизнеса, с использованием новейших цифровых технологий.</p>
+               </div>
+               <div class="support-about-stats">
+                    <div class="support-about-stats-text">
+                         <dl>
+                              <dt>100+ <span>тыс</span></dt>
+                              <dd>Клиентов<br /> по всему миру</dd>
+                         </dl>
+                         <dl>
+                              <dt>15 <span>лет</span></dt>
+                              <dd>Качественной работы</dd>
+                         </dl>
+                         <dl>
+                              <dt>50+</dt>
+                              <dd>Опытных юристов в штате</dd>
+                         </dl>
+                         <dl>
+                              <dt>SIRIUS</dt>
+                              <dd>Система на базе искусственного интеллекта</dd>
+                         </dl>
+                    </div>
+                    <div class="support-about-stats-img">
+                         <img src="<?php echo wp_get_upload_dir()['url'] . '/support-header-img.svg' ?>" alt="support-header-img">
+                    </div>
+               </div>
+          </div>
+          <div class="support-clients">
+               <p class="support-clients__title">Даём поддержку</p>
+               <div class="support-clients__client-card">
+                    <article>
+                         <div>
+                              <div>
+                                   <img src="<?php echo wp_get_upload_dir()['url'] . '/support-business.svg' ?>" alt="support-business">
+                              </div>
+                              <h3 class="text-supprt-card-title">Бизнесу</h3>
+                              <p class="text-supprt-card-subtitle">Услуги профессиональных юристов для вашей компании по фиксированному тарифу — это в несколько раз выгоднее, чем содержать свой штат.</p>
+                         </div>
+                    </article>
+                    <article>
+                         <div>
+                              <div>
+                                   <img src="<?php echo wp_get_upload_dir()['url'] . '/support-startUp.svg' ?>" alt="support-business">
+                              </div>
+                              <h3 class="text-supprt-card-title">StartUP проектам</h3>
+                              <p class="text-supprt-card-subtitle">Эксперты ILC помогут выбрать оптимальную правовую форму для ведения бизнеса, оптимизировать налоги и подготовить необходимый пакет документов.</p>
+                         </div>
+                    </article>
+                    <article>
+                         <div>
+                              <div>
+                                   <img src="<?php echo wp_get_upload_dir()['url'] . '/support-privateClients.svg' ?>" alt="support-privateClients">
+                              </div>
+                              <h3 class="text-supprt-card-title">Частным клиентам</h3>
+                              <p class="text-supprt-card-subtitle">Оперативные решения сложных вопросов в режиме онлайн: ДТП, наследство, страховые споры, земельные и имущественные процессы, защита прав.</p>
+                         </div>
+                    </article>
+               </div>
+          </div>
+     </section>
+     <section class="tariffs small-container">
+          <div class="tariffs_header">
+               <h2 class="tariffs_header_title">Тарифы</h2>
+               <p class="tariffs_header_subtitle">для всех и для каждого</p>
+          </div>
+          <div class="tariffs_time-toggle tariff_toggle-text">
+               <button class="tariff-toggle-active">1 месяц</button>
+               <button href="#">Год</button>
+          </div>
+          <ul class="tariffs_card-wrap">
+               <!-- <li class="tariffs_card tariffs_card--free">
+                    <div>
+                         <h3 class="tariffs_card_title"><?php the_title() ?></h3>
+                         <p class="tariffs_card_type-client"></p>
+                         <p class="tariffs_card_price">$28/месяц</p>
+                         <h4 class="tariffs_card_privilege-title">Для Вас доступно</h4>
+                         <ul class="tariffs_card_privilege">
+                              <li>Бесплатная телефонная линия + функция “перезвоните мне” </li>
+                              <li>Доступ к первому юридическому интеллекту Sirius 24/7</li>
+                              <li>Личный кабинет (полная версия)</li>
+                              <li>Конструктор документов</li>
+                              <li>Устные и письменные консультации юристов 24/7</li>
+                         </ul>
+                         <a class="tariffs_card_more-btn" href="#">Подробнее в личном кабинете</a>
+                         <a class="btn" href="#">Оставить заявку</a>
+                    </div>
+               </li> -->
+               <?php
+               global $post;
+
+               $myposts = get_posts([
+                    'post_type' => 'tariffs',
+                    'numberposts' => -1,
+                    'suppress_filters' => true,
+               ]);
+
+               if ($myposts) {
+                    foreach ($myposts as $post) {
+                         setup_postdata($post);
+               ?>
+                         <li class="tariffs_card">
+                              <div>
+                                   <h3 class="tariffs_card_title"><?php the_title() ?></h3>
+                                   <p class="tariffs_card_type-client"><?php the_field('type_client') ?></p>
+                                   <p class="tariffs_card_price"><?php the_field('price') ?></p>
+                                   <h4 class="tariffs_card_privilege-title">Для Вас доступно</h4>
+                                   <ul class="tariffs_card_privilege">
+                                        <?php 
+                                             for ($i=1; $i <=5 ; $i++) { 
+                                                  if (get_field('privilege'.$i)){
+                                                       $privilegeText = get_field('privilege'.$i);
+                                                       echo '<li>'.$privilegeText.'</li>';
+                                                  }
+                                             }
+                                        ?>
+                                   </ul>
+                                   <a class="tariffs_card_more-btn" href="#">Подробнее в личном кабинете</a>
+                                   <a class="btn" href="#">Оставить заявку</a>
+                              </div>
+                         </li>
+               <?php
+                    }
+               } else {
+                    // Постов не найдено
+               }
+
+               wp_reset_postdata(); // Сбрасываем $post
+               ?>
+
+
+               <!-- <li class="tariffs_card tariffs_card--free">
+                    <div>
+                         <h3 class="tariffs_card_title">Стандарт</h3>
+                         <p class="tariffs_card_type-client">Для физических лиц</p>
+                         <p class="tariffs_card_price">$28/месяц</p>
+                         <h4 class="tariffs_card_privilege-title">Для Вас доступно</h4>
+                         <ul class="tariffs_card_privilege">
+                              <li>Бесплатная телефонная линия + функция “перезвоните мне” </li>
+                              <li>Доступ к первому юридическому интеллекту Sirius 24/7</li>
+                              <li>Личный кабинет (полная версия)</li>
+                              <li>Конструктор документов</li>
+                              <li>Устные и письменные консультации юристов 24/7</li>
+                         </ul>
+                         <a class="tariffs_card_more-btn" href="#">Подробнее в личном кабинете</a>
+                         <a class="btn" href="#">Оставить заявку</a>
+                    </div>
+               </li> -->
+               <!--
+               <li class="tariffs_card">
+                    <div>
+                         <h3 class="tariffs_card_title">бизнес-консультант</h3>
+                         <p class="tariffs_card_type-client">Для частных лиц и фрилансеров</p>
+                         <p class="tariffs_card_price">$38/месяц</p>
+                         <h4 class="tariffs_card_privilege-title">Для Вас доступно</h4>
+                         <ul class="tariffs_card_privilege">
+                              <li>Бесплатная телефонная линия + функция “перезвоните мне” </li>
+                              <li>Доступ к первому юридическому интеллекту Sirius 24/7</li>
+                              <li>Пакет документов для открытия бизнеса в РФ</li>
+                              <li>Сопровождение сделок</li>
+                              <li>Устные и письменные консультации юристов 24/7</li>
+                         </ul>
+                         <a class="tariffs_card_more-btn" href="#">Подробнее в личном кабинете</a>
+                         <a class="btn" href="#">Оставить заявку</a>
+                    </div>
+               </li>
+               <li class="tariffs_card">
+                    <div>
+                         <h3 class="tariffs_card_title">бизнес-премиум</h3>
+                         <p class="tariffs_card_type-client">Для среднего бизнеса</p>
+                         <p class="tariffs_card_price">$49/месяц</p>
+                         <h4 class="tariffs_card_privilege-title">Для Вас доступно</h4>
+                         <ul class="tariffs_card_privilege">
+                              <li>Разработка уникальных договоров любой сложности</li>
+                              <li>Доступ к первому юридическому интеллекту Sirius 24/7</li>
+                              <li>Регистрация компаний и открытия счетов в РФ</li>
+                              <li>Сопровождение сделок любой сложности</li>
+                              <li>Устные и письменные консультации юристов 24/7</li>
+                         </ul>
+                         <a class="tariffs_card_more-btn" href="#">Подробнее в личном кабинете</a>
+                         <a class="btn" href="#">Оставить заявку</a>
+                    </div>
+               </li>
+               <li class="tariffs_card">
+                    <div>
+                         <h3 class="tariffs_card_title">международный бизнес</h3>
+                         <p class="tariffs_card_type-client">Для ведения бизнеса за рубежом</p>
+                         <p class="tariffs_card_price">$98/месяц</p>
+                         <h4 class="tariffs_card_privilege-title">Для Вас доступно</h4>
+                         <ul class="tariffs_card_privilege">
+                              <li>Разработка уникальных договоров любой сложности</li>
+                              <li>Консультации и сопровождение по регистрации компаний и банковских счетов по всему миру</li>
+                              <li>Оказание юридических услуг на английском языке</li>
+                              <li>Консультации по международному праву</li>
+                              <li>Сопровождение сделок любой сложности</li>
+                         </ul>
+                         <a class="tariffs_card_more-btn" href="#">Подробнее в личном кабинете</a>
+                         <a class="btn" href="#">Оставить заявку</a>
+                    </div>
+               </li> -->
+          </ul>
+     </section>
+     <section class="partner-banner">
+          <div class="partner-banner_text-wrap small-container">
+               <div class="partner-banner_text">
+                    <h2 class="partner-title">Станьте партнером ILC</h2>
+                    <p class="partner-subtitle">и зарабатывайте на рекомендациях в любое время, из любой точки мира</p>
+               </div>
+               <img src="<?php echo wp_get_upload_dir()['url'] . '/partner-people-img.svg' ?>" alt="partner-people-img">
+          </div>
+          <img src="<?php echo wp_get_upload_dir()['url'] . '/partners-banner-planet.jpg' ?>" alt="partners-banner-planet">
+     </section>
+     <section class="start small-container">
+          <h2>Начать легко</h2>
+          <div class="start_content-wrap">
+               <div class="start-content_card-block-wrap">
+                    <div class="start-content_card-wrap">
+                         <article class="start-content_card">
+                              <div>
+                                   <h3>Регистрация</h3>
+                                   <p class="text-description">Заполните простую форму для участия в программе и активируйте клиентский пакет.</p>
+                              </div>
+                         </article>
+                         <article class="start-content_card">
+                              <div>
+                                   <h3>Обучение</h3>
+                                   <p class="text-description">Вас ждут полезные обучающие материалы, с помощью которых вы легко сможете монетизировать свое время.</p>
+                              </div>
+                         </article>
+                         <article class="start-content_card">
+                              <div>
+                                   <img src="<?php echo wp_get_upload_dir()['url'] . '/start-strategy.svg' ?>" alt="start-strategy">
+                                   <h3>Стратегия</h3>
+                                   <p class="text-description">Воспользуйтесь подробным и понятным алгоритмом действий для достижения классных результатов.</p>
+                              </div>
+                         </article>
+                    </div>
+                    <article class="start-content_card--last">
+                         <div>
+                              <img src="<?php echo wp_get_upload_dir()['url'] . '/start-result.svg' ?>" alt="start-result">
+                              <h3>Результат</h3>
+                              <p class="text-description">Воспользуйтесь подробным и понятным алгоритмом действий для достижения классных результатов.</p>
+                              <a class="btn" href="#">Начать зарабатывать</a>
+                         </div>
+                    </article>
+               </div>
+               <img src="<?php echo wp_get_upload_dir()['url'] . '/start-bg.svg' ?>" alt="start-bg">
+          </div>
+     </section>
+     <section class="plans">
+          <div class="plans_content-wrap large-container">
+               <h2>Прогнозы партнерской программы на 2021 год</h2>
+               <div class="plans_stats-wrap">
+                    <dl>
+                         <dt>$2 млн.</dt>
+                         <dd>Заработают партнеры ILC</dd>
+                    </dl>
+                    <dl>
+                         <dt>10 000+</dt>
+                         <dd>Полисов будет оформлено по партнерским ссылкам</dd>
+                    </dl>
+                    <dl>
+                         <dt>800+</dt>
+                         <dd>Партнеров присоединится к нам</dd>
+                    </dl>
+                    <dl>
+                         <dt>$1500</dt>
+                         <dd>Средний доход партнера в месяц</dd>
+                    </dl>
+               </div>
+          </div>
+     </section>
+</main>
+<?php get_footer(); ?>
+<?php wp_footer(); ?>
+</div>
+</body>
+
+</html>
